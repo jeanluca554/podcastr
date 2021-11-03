6ea10f693b05a809ca62b9ae99c86fd9e3db42f0
Jean Luca <jean@jean-PC>
2021-11-03 07:27:48 -0300

    Play e pause funcionando

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -2,6 +2,7 @@ import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { GetStaticProps, GetStaticPaths } from 'next';
 
 import { api } from '../../services/api';
@@ -26,12 +27,13 @@ type EpisodeProps = {
 }
 
 export default function Episode({ episode }: EpisodeProps) {
+
     return (
         <div className={styles.episode}>
             <div className={styles.thumbnailContainer}>
                 <Link href="/">
                     <button type="button">
-                        <img src="/arrow-left.svg" alt="voltar" />
+                        <img src="/arrow-left.svg" alt="Voltar" />
                     </button>
                 </Link>
                 <Image
@@ -62,8 +64,24 @@ export default function Episode({ episode }: EpisodeProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+    const { data } = await api.get('episodes', {
+        params: {
+            _limit: 2,
+            _sort: 'published_at',
+            _order: 'desc'
+        }
+    })
+
+    const paths = data.map(episode => {
+        return {
+            params: {
+                slug: episode.id
+            }
+        }
+    })
+
     return {
-        paths: [],
+        paths,
         fallback: 'blocking'
     }
 }
